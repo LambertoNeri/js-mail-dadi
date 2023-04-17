690f6c870bf833298a56dcd523bb6e373a57f68a
LambertoNeri <neri.lamberto91@gmail.com>
2023-04-17 15:08:47 +0200

    start exercise + add all email

---
+++ mail/java/script.js
@@ -0,0 +1,40 @@
+/*
+Mail
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+
+Consigli del giorno:
+1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+2. javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
+3. si ma noi cosa vogliamo fare?
+4. torniamo a scrivere in italiano
+5. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"  */
+
+
+
+var email = prompt("Inserisci la tua email")
+let myArr = ["gigi@hotmail.it", "topolino@hotmail.it", "pilato@hotmail.it", "pelato@giorgio.com", "candidiocleziano@yahoo.it", "dedo@bimbo.com"]; 
+
+var bono = false
+let i = 0
+for (  i; i < myArr.length; i++) {
+    if (myArr[i] == email) {
+        bono = true
+    }
+}
+
+if (bono == true) {
+    console.log ("DAJE ROMA DAJE")
+} else {
+    console.log ("EMAIL NON PRESENTE NEL DATABASE")
+}
+
+
+
+
+
+
+
+
+
